6fd047b43c5aef556b1ccd2a251f68ec063e963b
Nicolò Rossi <nicolorossi8@gmail.com>
2021-03-13 20:44:27 +0100

    aggiunto la registrazione, il login e l'ottenimento del profilo

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,14 +1,18 @@
 import express from "express"
 import { Client } from "pg"
-import { Database } from "./Database"
+import { Utilita } from "./Database"
+import { utenteRoutes } from "./routes/utenteRoutes"
 const app = express()
 const porta = 5000
 
 app.use(express.json())
 
 
-Database.db.connect()
+Utilita.db.connect()
     .then(()=>console.log("database collegato con successo"))
     .catch((err)=>console.log(`impossibile collegarsi al db: ${err.message}`))
+
+
+app.use("/utenti/",utenteRoutes)
 
 app.listen(porta, ()=>console.log(`server aperto sulla porta ${porta}`))

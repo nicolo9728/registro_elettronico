3a4a67686a69e74adadb54f77b8185e026772a1b
Nicolò Rossi <nicolorossi8@gmail.com>
2021-03-18 14:59:36 +0100

    aggiunta la possibilita di vedere i voti

--- api/src/routes/studenteRoutes.ts
+++ api/src/routes/studenteRoutes.ts
@@ -1,4 +1,18 @@
 import { Router } from "express";
+import { Pool } from "pg";
+import { controlloLoggato } from "../middleware/controlloLoggato";
+import { controlloStudente } from "../middleware/controlloStudente";
 
 export const studenteRoutes = Router()
 
+studenteRoutes.get("/voti", controlloLoggato, controlloStudente, async (req ,res)=>{
+    const matricola = req.body.utenteLoggato.matricola
+
+    try{
+        const ris = await new Pool().query("SELECT valutazione, data, nomeMateria, nome as nomeDocente FROM VOTI natural join Docenti where idStudente=$1", [matricola])
+        res.status(200).send(ris.rows)
+    }
+    catch(e){
+        res.status(500).send("impossibile ottenere i voti")
+    }
+})

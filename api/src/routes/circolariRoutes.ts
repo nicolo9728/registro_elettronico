6ceff059e64c00aed4c1d891b56ed79095f9a61b
Nicolò Rossi <nicolorossi8@gmail.com>
2021-04-13 18:55:12 +0200

    implementazione della sede

--- api/src/routes/circolariRoutes.ts
+++ api/src/routes/circolariRoutes.ts
@@ -1,7 +1,14 @@
 import { Router } from "express";
+import { Pool } from "pg";
 
-const circolariRoutes = Router()
+export const circolariRoutes = Router()
 
-circolariRoutes.get("/", (req, res)=>{
-    
+circolariRoutes.get("/", async (req, res)=>{
+    let circolari;
+    if(req.query.nomeSede)
+        circolari = await new Pool().query("select numero, titolo, contenuto, nomesede from Circolari where nomeSede=$1", [req.query.nomeSede]);
+    else
+        circolari = await new Pool().query("select numero, titolo, contenuto, nomesede from Circolari");
+
+    res.status(200).json(circolari.rows)
 })

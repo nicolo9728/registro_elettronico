a4bf79ad5587039f1e2bec9e3fa352cb717c1c14
Nicolò Rossi <nicolorossi8@gmail.com>
2021-04-21 18:55:55 +0200

    aggiornate le api

--- api/src/routes/studenteRoutes.ts
+++ api/src/routes/studenteRoutes.ts
@@ -17,3 +17,24 @@ studenteRoutes.get("/voti", controlloLoggato, controlloStudente, async (req ,res
         res.status(500).send("impossibile ottenere i voti")
     }
 })
+
+studenteRoutes.get("/ottieniEventi", controlloLoggato, controlloStudente, async (req, res)=>{
+    const data = req.query.data
+    const matricola = req.body.utenteLoggato.matricola
+    const pool = new Pool()
+
+    try{
+        const queryVoti = await pool.query("select valutazione, descrizione, data, nomeMateria, nome as nomeDocente from Voti natural join Docenti where idStudente=$1 and data=$2", [matricola, data])
+        const queryPresenze = await pool.query("select data, nome as nomeDocente, cognome as cognomeDocente from presenze natural join Docenti where idStudente=$1 and data=$2", [matricola, data])
+
+        const risposta = {
+            voti: queryVoti.rows,
+            presenze: queryPresenze.rows
+        }
+
+        res.status(200).json(risposta)
+    }
+    catch(e){
+        res.status(400).send(e.message)
+    }
+})

b81493e525bffd852daaa0bb9c67b173889ed2dd
Nicolò Rossi <nicolorossi8@gmail.com>
2021-05-08 13:01:54 +0200

    fix

--- api/src/routes/studenteRoutes.ts
+++ api/src/routes/studenteRoutes.ts
@@ -10,11 +10,11 @@ studenteRoutes.get("/voti", controlloLoggato, controlloStudente, async (req ,res
     const matricola = req.body.utenteLoggato.matricola
 
     try{
-        const ris = await new Pool(dbImpostazioni).query("SELECT valutazione, data, nomeMateria, descrizione, nome FROM VOTI natural join Docenti where idStudente=$1", [matricola])
+        const ris = await new Pool(dbImpostazioni).query("SELECT valutazione, data, nomeMateria, descrizione FROM VOTI natural join Docenti where idStudente=$1", [matricola])
         
         res.status(200).json(ris.rows)
     }
     catch(e){
-        res.status(500).send("impossibile ottenere i voti")
+        res.status(500).send(e.message)
     }
 })
